346baabbd0d55a95be033d7855f9c1c5c64ae65e
Alexandr Burakov <bursasha1201@gmail.com>
2022-08-06 01:43:45 +0200

    Adding 5th section Design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+	$('.design__slider').slick({
+		dots: false,
+		slidesToShow: 4,
+		variableWidth: true,
+		prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="Left arrow">',
+		nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="Right arrow">',
+	});
 });
