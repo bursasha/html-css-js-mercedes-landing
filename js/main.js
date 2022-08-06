bd926bbdc0326e88d334f7ba8aeac1f3b73acd75
Alexandr Burakov <bursasha1201@gmail.com>
2022-08-06 11:30:20 +0200

    Adding adaptive layout and fixes

--- js/main.js
+++ js/main.js
@@ -5,5 +5,12 @@ $(function(){
 		variableWidth: true,
 		prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="Left arrow">',
 		nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="Right arrow">',
+		responsive: [{
+			breakpoint: 361,
+			settings: {
+				variableWidth: false,
+				slidesToShow: 1,
+			}
+		}]
 	});
 });
